467c204a519f66be18bef04094b702c8d0fd24f7
Sapnendra Jaiswal <116286507+Sapnendra@users.noreply.github.com>
2022-12-17 18:08:08 +0530

    Add files via upload
    Suppose, string = " [ { ( ) } ] "   :- this is valid parenthesis
    and          string = " [{ ( } ) ) { } ] " :- this is Not valid parenthesis

---
+++ Stack_Valid_Paranthesis.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValidParanthesis(string expression) {
+    stack<char> s;
+    for( int i = 0; i < expression.length(); i++) {
+        char ch = expression[i];
+
+        // if opening bracket, stack push
+        // if closing bracket, stacktop closer and pop
+        if( ch == '(' || ch == '{' || ch == '[' ) {
+            s.push(ch);
+        } else {
+            // for closing bracket
+            if(!s.empty()) {
+                char top = s.top();
+                if( (ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[') )
+                {
+                    s.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+    }
+    if(s.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    string str = "[{)}]";
+    if(isValidParanthesis(str)) {
+        cout << "Valid paranthesis" << endl;
+    } else {
+        cout <<"Not valid paranthesis" << endl;
+    }
+    return 0;
+}
